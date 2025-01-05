6a5e73354b578fd184ccc43b8b5f4ded4d54fa38
Jacob Schnettler <jacobschnettler12@gmail.com>
2025-01-05 01:50:11 -0500

    Microcontroller

---
+++ microcontroller/index/index.ino
@@ -0,0 +1,45 @@
+// Dashcam microcontroller
+// Written By Jacob Schnettler
+// 1/5/25
+
+boolean machinePower = false;
+
+boolean recording = false;
+
+void setup() {}
+
+void startRecording() {
+  
+}
+
+void stopRecording() {
+
+}
+
+void toggleRecording() {
+  if (recording) {
+    stopRecording();
+  } else {
+    startRecording();  
+  }
+}
+
+void powerOn() {
+
+}
+
+void powerOff() {
+
+}
+
+void powerPush() {
+
+}
+
+void loop() {
+  // Listen for power on/off
+  // powerPush();
+  
+  // Listen for Rec stop/start
+  // toggleRecording();
+}
